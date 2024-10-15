1a52ac38816ec14505f637a55e1ff8e04127ebf3
Thanatorn Chunpimpanon <2310511105009@live4.utcc.ac.th>
2024-10-15 17:00:30 +0700

    sprite animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator animator;
     [SerializeField] float speed = 1000f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 scale = transform.localScale;
+
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector3(-speed * Time.deltaTime, 0);
+            scale.x = -1;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector3(speed * Time.deltaTime, 0);
+            scale.x = 1;
+        }
+
+        animator.SetBool("isWalking", true);
+        transform.localScale = scale;
+
+        if (Mathf.Abs(rb.velocity.x) < 0.5)
+        {
+            animator.SetBool("isWalking", false);
         }
     }
 }
